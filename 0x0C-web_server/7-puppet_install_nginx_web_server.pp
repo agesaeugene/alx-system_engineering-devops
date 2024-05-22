b9690364f7661b02c0f9f692c8c567311865e1e4
Eugene Agesa <eugeneagesa734@gmail.com>
2024-05-22 00:55:57 -0700

    puppet file updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,24 @@
 # automating web service using puppet
 
+exec { 'update system':
+	command => '/usr/bin/apt-get update',
+}
+
 package { 'nginx':
-  ensure => installed,
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-file_line { 'install':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https:/www.github.com/agesaeugene permanent;',
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello world:',
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.github.com/agesaeugene;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-service { 'nginx':
- ensure  => running,
- require => package['nginx'],
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
