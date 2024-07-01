52bd520c69456eb4bb8c83a8c11eabbb1c712f40
Eugene Agesa <eugenethebandit@gmail.com>
2024-07-01 02:55:19 -0700

    flask intsallation errors fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
 # install a specific  version of flask(2.1.0)
 
+package { 'python3-pip':
+  ensure => installed,
+}
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
+
 }
