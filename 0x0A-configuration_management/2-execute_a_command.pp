49ac96fa02fdf479fc352456e09f7eee57b07c35
Eugene Agesa <eugenethebandit@gmail.com>
2024-07-01 05:20:36 -0700

    pklill task updated

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # Executes a command
-exec { 'pkill killmenow':
-	path => '/usr/bin:/usr/sbin:/bin'
+exec { 'kill_process':
+  command  => 'pkill -f killmenow',
+  provider => 'shell',
+  path     => ['/usr/bin', '/usr/sbin',]
 }
-
