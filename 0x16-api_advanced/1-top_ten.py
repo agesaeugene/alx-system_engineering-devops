5d5ec05d04f06dd1636e9c3897fe76d9abd49ddd
Eugene Agesa <eugenethebandit@gmail.com>
2024-08-13 15:52:27 -0700

    top ten code update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,19 +2,21 @@
 """Function for printing hot posts on a specific Reddit subreddit."""
 import requests
 
+
 def top_ten(subreddit):
-    """titles of the 10 hottest posts on a given subreddit are printed"""
+    """Titles of the 10 hottest posts on a given subreddit are printed"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/eugene)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/eugene)"
     }
     params = {
-            "limits": 10
+        "limit": 10
     }
     response = requests.get(url, headers=headers, params=params,
-            allow_redirects=False)
+                            allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
     results = response.json().get("data")
-    [print(c.get"data").get"title")) for c in results.get("children")]
+    [print(c.get("data").get("title")) for c in results.get("children")]
+
